f67828fcb5b33ab2eb156839bd2a3b67b6871212
Daniel Miladinov <dmiladinov@wingspan.com>
2013-10-09 23:24:02 -0400

    Update project dependencies to include clojurescript

--- project.clj
+++ project.clj
@@ -3,4 +3,27 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+
+  ;; CLJ source code path
+  :source-paths ["src/clj"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1913"]]
+
+  ;; lein-cljsbuild plugin to build a CLJS project
+  :plugins [[lein-cljsbuild "0.3.3"]]
+
+  ;; cljsbuild options configuration
+  :cljsbuild {:builds
+              [{;; CLJS source code path
+                :source-paths ["src/cljs"]
+
+                ;; Google Closure (CLS) options configuration
+                :compiler {;; CLS generated JS script filename
+                           :output-to "resources/public/js/modern.js"
+
+                           ;; minimal JS optimization directive
+                           :optimizations :whitespace
+
+                           ;; generated JS code prettyfication
+                           :pretty-print true}}]})
+
